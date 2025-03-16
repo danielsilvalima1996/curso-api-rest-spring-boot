65c9153658a28e40fcd8f1d9e423ffab959876ac
danielsilvalima1996 <dsl15021996@gmail.com>
2025-03-15 21:28:13 -0300

    finalizado modulo 07 - verbos HTTP

--- projetos/rest-with-spring-boot-and-java-session-7/src/main/java/br/com/daniel/controllers/PersonController.java
+++ projetos/rest-with-spring-boot-and-java-session-7/src/main/java/br/com/daniel/controllers/PersonController.java
@@ -1,4 +1,49 @@
 package br.com.daniel.controllers;
 
+import br.com.daniel.model.Person;
+import br.com.daniel.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
 public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") String id) {
+        return service.findById(id);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findById() {
+        return service.findAll();
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteById(@PathVariable("id") String id) {
+        service.deleteById(id);
+    }
+
 }
